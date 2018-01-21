055f5f8450f6e7f779db1329f16f128dee18de7f
Paul Knopf <pauldotknopf@gmail.com>
2018-01-21 16:56:21 -0500

    Finally able to build a valid image!

--- repository/build.go
+++ repository/build.go
@@ -1,30 +1,32 @@
 package repository
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"path"
 	"runtime"
-	"encoding/json"
-	"bytes"
 
 	"github.com/opencontainers/image-spec/identity"
 
 	"github.com/containerd/containerd"
 
+	"github.com/containerd/containerd/content"
+	"github.com/containerd/containerd/diff"
+	"github.com/containerd/containerd/images"
 	"github.com/containerd/containerd/namespaces"
 	"github.com/containerd/containerd/oci"
 	"github.com/containerd/containerd/reference"
+	digest "github.com/opencontainers/go-digest"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pauldotknopf/darch/recipes"
 	"github.com/pauldotknopf/darch/utils"
-	"github.com/containerd/containerd/diff"
 	"github.com/pauldotknopf/darch/workspace"
-	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
-	"github.com/containerd/containerd/images"
-	"github.com/containerd/containerd/content"
-	digest "github.com/opencontainers/go-digest"
 )
+
+const containerdUncompressed = "containerd.io/uncompressed"
 
 // BuildRecipe Builds a recipe.
 func (session *Session) BuildRecipe(ctx context.Context, recipe recipes.Recipe, tag string, buildPrefix string, environmentVariables map[string]string) error {
@@ -134,7 +136,7 @@ func (session *Session) BuildRecipe(ctx context.Context, recipe recipes.Recipe, 
 		return err
 	}
 
-	return session.createImageFromSnapshot(ctx, img, snapshotKey, recipe.Name + ":" + tag)
+	return session.createImageFromSnapshot(ctx, img, snapshotKey, recipe.Name+":"+tag)
 }
 
 func (session *Session) createSnapshot(ctx context.Context, snapshotKey string, img containerd.Image) error {
@@ -151,6 +153,56 @@ func (session *Session) createSnapshot(ctx context.Context, snapshotKey string, 
 
 func (session *Session) deleteSnapshot(ctx context.Context, snapshotKey string) error {
 	return session.client.SnapshotService(containerd.DefaultSnapshotter).Remove(ctx, snapshotKey)
+}
+
+func (session *Session) patchImageConfig(ctx context.Context, ref string, manifest *ocispec.Manifest, newLayerDigest digest.Digest) error {
+	// Get the current image configuration.
+	p, err := content.ReadBlob(ctx, session.client.ContentStore(), manifest.Config.Digest)
+	if err != nil {
+		return err
+	}
+
+	// Deserialize the image configuration to a generic json object.
+	// We do this so that we can patch it, without requiring knowledge
+	// of the entire schema.
+	m := map[string]json.RawMessage{}
+	if err = json.Unmarshal(p, &m); err != nil {
+		return err
+	}
+
+	// Pull the rootfs section out, so that we can append a layer to the diff_ids array.
+	var rootFS ocispec.RootFS
+	p, err = m["rootfs"].MarshalJSON()
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(p, &rootFS); err != nil {
+		return err
+	}
+	rootFS.DiffIDs = append(rootFS.DiffIDs, newLayerDigest)
+	p, err = json.Marshal(rootFS)
+	if err != nil {
+		return err
+	}
+	m["rootfs"] = p
+
+	// Convert our entire image configuration back to bytes, and write it to the content store.
+	p, err = json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	manifest.Config.Digest = digest.FromBytes(p)
+	err = content.WriteBlob(ctx, session.client.ContentStore(),
+		ref,
+		bytes.NewReader(p),
+		int64(len(p)),
+		manifest.Config.Digest,
+	)
+	if err != nil {
+		return err
+	}
+
+	return err
 }
 
 func (session *Session) createImageFromSnapshot(ctx context.Context, img containerd.Image, activeSnapshotKey string, newReference string) error {
@@ -191,6 +243,7 @@ func (session *Session) createImageFromSnapshot(ctx context.Context, img contain
 	}
 	defer snapshotService.Remove(ctx, "temp-readonly-parent")
 
+	// Generate a diff in content store
 	diffs, err := session.client.DiffService().DiffMounts(ctx,
 		lowerMounts,
 		upperMounts,
@@ -199,8 +252,30 @@ func (session *Session) createImageFromSnapshot(ctx context.Context, img contain
 	if err != nil {
 		return err
 	}
+
 	// Add our new layer to the image manifest
 	manifest.Layers = append(manifest.Layers, diffs)
+
+	// Add the blob checksum to image config
+	info, err := contentStore.Info(ctx, diffs.Digest)
+	if err != nil {
+		return err
+	}
+	diffIDStr, ok := info.Labels[containerdUncompressed]
+	if !ok {
+		return fmt.Errorf("invalid differ response with no diffID")
+	}
+	diffIDDigest, err := digest.Parse(diffIDStr)
+	if err != nil {
+		return err
+	}
+	err = session.patchImageConfig(ctx, "custom-ref", &manifest, diffIDDigest)
+	if err != nil {
+		return err
+	}
+
+	// Save our new image manifest, which now hows our new layer,
+	// and a patched image config with a reference to the new layer.
 	manifestBytes, err := json.Marshal(manifest)
 	if err != nil {
 		return err
@@ -212,24 +287,35 @@ func (session *Session) createImageFromSnapshot(ctx context.Context, img contain
 		bytes.NewReader(manifestBytes),
 		int64(len(manifestBytes)),
 		manifestDigest); err != nil {
-			return err
+		return err
 	}
 
 	// Let's see if the image exists already, if so, let's delete it
-	_, err = session.client.ImageService().Get(ctx, newReference)
+	_, err = session.client.GetImage(ctx, newReference)
 	if err == nil {
 		session.client.ImageService().Delete(ctx, newReference, images.SynchronousDelete())
 	}
 
 	_, err = session.client.ImageService().Create(ctx,
 		images.Image{
-			Name:   newReference,
+			Name: newReference,
 			Target: ocispec.Descriptor{
 				Digest:    manifestDigest,
 				Size:      int64(len(manifestBytes)),
 				MediaType: ocispec.MediaTypeImageManifest,
 			},
 		})
+	if err != nil {
+		return err
+	}
+
+	// This will create the required snapshot for the new layer,
+	// which will allow us to run the image immediately.
+	newImage, err := session.client.GetImage(ctx, newReference)
+	if err != nil {
+		return err
+	}
+	err = newImage.Unpack(ctx, containerd.DefaultSnapshotter)
 	if err != nil {
 		return err
 	}
