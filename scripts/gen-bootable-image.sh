e44b45bb4120f3803b4005bf6014b2ef02604da9
Paul Knopf <pauldotknopf@gmail.com>
2018-11-07 23:36:28 -0500

    Increase size of Darch image. Setup fstab hooks. Add a link to the recipes on the desktop

--- scripts/gen-bootable-image.sh
+++ scripts/gen-bootable-image.sh
@@ -13,19 +13,19 @@ fi
 
 # Create our hard disk
 rm -rf boot.img
-truncate -s 10G boot.img
+truncate -s 20G boot.img
 parted -s boot.img \
         mklabel msdos \
         mkpart primary 0% 2GiB \
-        mkpart primary 2GiB 3GiB \
-        mkpart primary 3GiB 6GiB \
-        mkpart primary 6GiB 100%
+        mkpart primary 2GiB 2.5GiB \
+        mkpart primary 2.5GiB 3GiB \
+        mkpart primary 3GiB 100%
 
 # Partition layout:
 # 1. The base recovery os
 # 2. Darch configuration (/etc/darch)
+# 3. Home directory
 # 3. Darch stage/images
-# 4. Home directory
 
 # Mount the newly created drive
 loop_device=`losetup --partscan --show --find boot.img`
@@ -41,16 +41,22 @@ rm -rf rootfs && mkdir rootfs
 mount ${loop_device}p1 rootfs
 mkdir -p rootfs/etc/darch
 mount ${loop_device}p2 rootfs/etc/darch
-mkdir -p rootfs/var/lib/darch
-mount ${loop_device}p3 rootfs/var/lib/darch
 mkdir rootfs/home
-mount ${loop_device}p4 rootfs/home
+mount ${loop_device}p3 rootfs/home
+mkdir -p rootfs/var/lib/darch
+mount ${loop_device}p4 rootfs/var/lib/darch
 
 # Generate the rootfs
 debootstrap --verbose \
     --unpack-tarball=$(pwd)/debs.tar.gz \
     --include=linux-image-amd64,grub2 \
     stable rootfs https://deb.debian.org/debian
+
+# Generate fstab (removing comments and whitespace)
+genfstab -U -p rootfs | sed -e 's/#.*$//' -e '/^$/d' > rootfs/etc/fstab
+
+# Set the computer name
+echo "darch-demo" > rootfs/etc/hostname
 
 # Update all the packages
 arch-chroot rootfs apt-get update
@@ -78,17 +84,19 @@ arch-chroot rootfs mkdir -p /etc/containerd
 echo "root = \"/var/lib/darch/containerd\"" > rootfs/etc/containerd/config.toml
 arch-chroot rootfs systemctl enable containerd
 
+# Setup the fstab hooks for Darch
+cat rootfs/etc/fstab | tail -n +2 > rootfs/etc/darch/hooks/default_fstab
+echo "*=default_fstab" > rootfs/etc/darch/hooks/fstab.config
+
 # Run grub-mkconfig again to ensure it loads the Darch grub config file
 arch-chroot rootfs grub-mkconfig -o /boot/grub/grub.cfg
 
 # Clone our examples repo
-arch-chroot rootfs mkdir /home/darch/Desktop
 arch-chroot rootfs apt-get -y install git
 arch-chroot rootfs git clone https://github.com/godarch/example-recipes.git /home/darch/example-recipes
+arch-chroot rootfs mkdir /home/darch/Desktop
+arch-chroot rootfs ln -s /home/darch/example-recipes /home/darch/Desktop/Recipes
 arch-chroot rootfs chown -R darch:darch /home/darch/
-
-# Generate fstab
-genfstab -U -p rootfs > rootfs/etc/fstab
 
 # Clean up
 umount rootfs/etc/darch
